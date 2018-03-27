a2255d22a4d15b1ef850ab39d25c8e32ea8c79b8
Timothy Downs <timothy.steven.downs@gmail.com>
2018-03-27 07:30:11 +0200

    added app

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var config = require('./config/database');
+
+//connect to // DB
+mongoose.connect(config.database);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log('connected to MongoDB');
+});
+
+//init app
+var app = express();
+
+//pretyfy json
+app.set('json spaces', 40);
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extend: false }))
+app.use(bodyParser.json())
+
+// start server
+var port = 3002
+app.listen(port, function(){
+  console.log('server running at '+port);
+})
